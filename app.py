8233c9d3a3a3633ef9f5a0645aa77413259479b3
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-04-02 23:23:41 +0700

    feat: add detail of each pet

--- app.py
+++ app.py
@@ -1,37 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
+
+pets = [
+    {
+        "id": 1,
+        "name": "Nelly",
+        "age": "5 weeks",
+        "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles.",
+    },
+    {
+        "id": 2,
+        "name": "Yuki",
+        "age": "8 months",
+        "bio": "I am a handsome gentle-cat. I like to dress up in bow ties.",
+    },
+    {
+        "id": 3,
+        "name": "Basker",
+        "age": "1 year",
+        "bio": "I love barking. But, I love my friends more.",
+    },
+    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
+]
 
 
 @app.route("/")
 def home():
-    pets = [
-        {
-            "id": 1,
-            "name": "Nelly",
-            "age": "5 weeks",
-            "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles.",
-        },
-        {
-            "id": 2,
-            "name": "Yuki",
-            "age": "8 months",
-            "bio": "I am a handsome gentle-cat. I like to dress up in bow ties.",
-        },
-        {
-            "id": 3,
-            "name": "Basker",
-            "age": "1 year",
-            "bio": "I love barking. But, I love my friends more.",
-        },
-        {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
-    ]
     return render_template("home.html", pets=pets)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/details/<int:pet_id>")
+def pet_details(pet_id):
+    pet = next(filter(lambda pet: pet["id"] == pet_id, pets), None)
+    if pet is None:
+        abort(404, description="No Pet was Found with the given ID")
+    else:
+        return render_template("detail.html", pet=pet)
 
 
 if __name__ == "__main__":
